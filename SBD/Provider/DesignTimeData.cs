29bdce71afca27a30e37f4f9ed514e29a0d9390a
chichen0104 <chichen0104@gmail.com>
2024-08-21 21:53:47 +0800

    up

--- SBD/Provider/DesignTimeData.cs
+++ SBD/Provider/DesignTimeData.cs
@@ -1,6 +1,37 @@
-﻿namespace SBD.Provider
+﻿using DataLibrary;
+
+namespace SBD.Provider
 {
-    internal class DesignTimeData
+    public static class DesignTimeData
     {
+        public static BoardingPassData BoardingPassData { get; set; }
+
+        static DesignTimeData()
+        {
+            bool isDesignTime = true;
+
+            if (isDesignTime)
+            {
+                BoardingPassData = GenerateBoardingPassData();
+
+            }
+        }
+
+        private static BoardingPassData GenerateBoardingPassData()
+        {
+            return new BoardingPassData
+            {
+                PassengerName = null,
+                FlightNumber = null,
+                SeatNumber = null,
+                DepartureAirport = null,
+                ArrivalAirport = null,
+                DepartureTime = null,
+                ArrivalTime = null,
+                BoardingGate = null,
+                TicketNumber = null,
+                LuggageList = null
+            };
+        }
     }
 }
